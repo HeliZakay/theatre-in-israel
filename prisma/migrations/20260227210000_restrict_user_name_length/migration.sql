188e21e5a89b2e5c01e95a75ad4df4b86be0ce77
Heli <helizakay1@gmail.com>
2026-02-27 16:05:29 +0200

    fix: truncate long user names before restricting column to VARCHAR(20)

--- prisma/migrations/20260227210000_restrict_user_name_length/migration.sql
+++ prisma/migrations/20260227210000_restrict_user_name_length/migration.sql
@@ -1,2 +1,5 @@
+-- Truncate existing names that exceed the new limit
+UPDATE "User" SET name = LEFT(name, 20) WHERE LENGTH(name) > 20;
+
 -- AlterTable
 ALTER TABLE "User" ALTER COLUMN "name" SET DATA TYPE VARCHAR(20);
