deb5cb2079997d51f13650ec9a3359d587dea6e1
Heli <helizakay1@gmail.com>
2026-03-01 14:55:29 +0200

    Fix: Ensure review images are properly contained on mobile by setting position: relative for .poster in CSS

---
+++ prisma/migrations/20260301140000_backfill_hebrew_theatre_cast_data/migration.sql
@@ -0,0 +1,50 @@
+-- Backfill cast data for התיאטרון העברי (Hebrew Theatre) shows.
+-- Cast strings extracted from individual show pages at teatron.org.il.
+-- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, דין הפנר, שרון מלכי, פיני טבגר, מיכאל וילוז׳ני.' WHERE "slug" = 'זוגיות-AI';
+
+UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גורנשטיין, אלון אופיר, פנינה ברט צדקה, נמרוד גרינבוים, קארין שיפרין, נירית אהרוני, ניר שיבר, ארז וייס, עמית בורשטיין, יניב לוי, מירית פרג''ון, ענאל שדה, אדיר פלדבלום, אורי זילברשטיין, גאיה אלבס, דור ירדני, חן כהן, טל אליאס, נועה קאירי , נועם אלדר, נטעלי חכם, ניתאי א?- Backfill cast data for התיאטרון העברי (Hebrew Theatre) shows.
+-- Cast strings extracted from individual show pages at teatron.org.ilho-- Cast strings extracted from individual show pages at teatron.org.il.
+-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גורנשטיין, אלון אופיר, פנינה ברט צדקה, נמרוד גרינבוים?ו-- Cast strings extracted from individual show pages at teatron.org.ilho-- Cast strings extracted from individual show pages at teatron.org.il.
+-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גורנשטיין, אלון אופיר, פנינה ברט צדקה, נמרוד גרינבוים?ו-- Cast strings extracted from individual show pages at teatron.org.ilho-- Cast strings extracted from individual show pages at teatron.org.il.
+-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'י?'-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גי-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גורנשטיין, אלון אופיר, פנינה ברט צדקה, נמרוד גרינבוים?ו-- Cast strings extracted from individual s ?UPDATE "Show" SET "cas,גUPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גT -- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'י?'-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גי-- 2??- ?'
+UPDATE "Show" SET "casast
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ??
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גורנשטיין, אלד?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי ג??UPDATE "Show" SET "cast" = 'י?'-- 2??- 22 shows total.
+
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גי-- 2??- ?'
+UPDATE "Show" SET "casast
+UPDATE "Show" SET "ca?י
+UPDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?UUPDATE "Show" SET "casast
+UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנן UPDATE "Show" SET "cast"??PDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צ 'UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי ג ?UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גי-- 2???נUPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ??UPDATE "Show" SET "casast
+UPDATE "Show" SET "ca?י
+UPDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר????PDATE "Show" SET "ca?יצUPDATE "Show" SET "castרUPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנן UPDATE "Show" SET "cast"" UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צ 'UPDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי ג ?UPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?UPDATE "Show" SEow"
+UPDATE "Show" SET , מיUPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גי-- 2???נUPDATE "Show" SET "cast" = 'נתן דטנר??UPD ?PDATE "Show" SET "ca?י
+UPDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר????PDATE "Show" SET "ca?יצUPDATE "Show" SET "castרUPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show".'UPDATE "Show" SET "castלUPDATE "Show" SET , מיUPDATE "Show" SET "cast" = 'נתן דטנר??UPDATE "Show" SET "casנ?PDATE "Show" SET "cast" = 'נתן דטנר, מיקה צור, אלי גי-- 2???נUPDATE "Show" SET "cast" = 'נתן דטנר??UPD ?PDATE "Show" SET "ca?י
+UPDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר????PDATE "Show" SET "ca?יצUPDATE "Show" SET "castרUPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show".'UPDATE "Show" SET "castלUPDA?כא?PDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר????PDATE "Show" SET "ca?יצUPDATE "Show" SET "castרUPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show".'UPDATE "Show" SET "cast??UPDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר????PDATE "Show" SET "ca?יצUPDATE "Show" SET "castרUPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show".'UPDATE "Show" SET "castלUPDA?כא?PDATE "Show" SET "cast" = 'יגאל עדי, ספיר ב?PDATE "Show" SET "cast" = 'נתן דטנר????PDATE "Show" SET "ca?יצUPDATE "Show" SET "castרUPDATE "Show" SET "cast" = 'יגאל עדיקא, ת?Uי?PDATE "Show".'UPDATE "Show" ס / דבי יבלונקה (בתפקיד רוחל''ה).' WHERE "slug" = 'זוג-משמיים';
+
+UPDATE "Show" SET "cast" = 'נתן דטנר , פולי רשף, נטע פלוטניק, עינבר דנון ,רוי מילר' WHERE "slug" = 'רוחל׳ה-מתחתנת';
