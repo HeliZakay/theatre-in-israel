c20080f43d4052fb64d0df7ae75887daadf39d64
Heli <helizakay1@gmail.com>
2026-03-01 16:28:49 +0200

    Backfill script and migration for Tzavta cast data

---
+++ prisma/migrations/20260301160000_backfill_tzavta_cast_data/migration.sql
@@ -0,0 +1,26 @@
+
+-- Backfill actors-only cast data for Tzavta Theatre (תיאטרון צוותא) shows
+-- Cast extracted from individual show pages at tzavta.co.il
+-- Format: comma-separated actor names, / for alternates
+-- 20 shows updated
+
+UPDATE "Show" SET "cast" = 'מיקה זק, יונתן שמעוני' WHERE "slug" = 'Mix-&-Match';
+UPDATE "Show" SET "cast" = 'נגה בן נר, רועי דרור, רון חכמון, יונתן סבוראי, שחר סיטון, לילי פרלמן' WHERE "slug" = 'זה-בסדר.';
+UPDATE "Show" SET "cast" = 'עמרי הכהן, אילון פרבר, רני אלון, שרי עציץ, עידו גולן ואוריה ג''ורג''י' WHERE "slug" = 'שיר-של-אמונה-וכפירה';
+UPDATE "Show" SET "cast" = 'אבי אופיר וטס מאיר' WHERE "slug" = 'ככה-יצאתי';
+UPDATE "Show" SET "cast" = 'אביגיל אדרי, קובי ירחי, הילה קרני, אלעד עובדיה' WHERE "slug" = 'פאק-בסיסטם';
+UPDATE "Show" SET "cast" = 'אורן הגני, אילון פרבר, אמיר משיח, אמלי כהן, בתשבע אוטולנגי, גל דאי, דניאל עובדיה, ישראל פניאל, נתנאל רפפורט, שרי עציץ' WHERE "slug" = 'בוז׳ולה';
+UPDATE "Show" SET "cast" = 'עידן אלתרמן, ג׳וי ריגר / דנה פרידר, אבי דנגור, הראל ליסמן / עמית רייס , רפאל עבאס , מרב צברי / עופרי סלומון, אביב הורוביץ, גיא וינטרוב / אלעד פרץ, אלעד טל / דורון ברנס' WHERE "slug" = 'הסוף';
+UPDATE "Show" SET "cast" = 'שרון שטרק, יואב בר לב, מאיה בכובסקי' WHERE "slug" = 'דמות-אב';
+UPDATE "Show" SET "cast" = 'מיקה זק, זיו תשובה, ירדן בר-אל, אביב בן' WHERE "slug" = 'שושי-בחלל';
+UPDATE "Show" SET "cast" = 'אוריה לימור קומורניק, יובל קרסל, אופק אהרוני' WHERE "slug" = 'הפנס-הסגול';
+UPDATE "Show" SET "cast" = 'מיכל לוסי גורפיין, גל ברק, יונתן בלומנפלד ואתי וקנין־סובר' WHERE "slug" = 'אנטי-אייג׳ינג';
+UPDATE "Show" SET "cast" = 'שיר בנימין, יותם פולישוק , יובל אנדבלד' WHERE "slug" = 'פצעים';
+UPDATE "Show" SET "cast" = 'טלי אוסדצ''י (אור), מיכל ויינברג (ספק), שני צברי (חושך), אליסה וייסבורד (רצון)' WHERE "slug" = 'אורות-קטנות';
+UPDATE "Show" SET "cast" = 'סלי עדן בר, יותם רוטשטיין, עידו שטרסבורג / גיא נטאף' WHERE "slug" = 'התחנה-האחרונה';
+UPDATE "Show" SET "cast" = 'דניאל זהבי, לילך צור' WHERE "slug" = 'משחקי-נישואים';
+UPDATE "Show" SET "cast" = 'איה חיות, גיא רון, גלי אשכנזי לוין, דורון גוברמן, זהר איזנמן, חן גבעתי, יהלי פרל, נדב מנור, עומר רוזנבלום, שחף קהלאני' WHERE "slug" = 'תיכון-מגשימים';
+UPDATE "Show" SET "cast" = 'אורי אוריין, טל בלנקשטיין פלג, עמרי הכהן, אתי וקנין סובר, יריב קוק' WHERE "slug" = 'מקליד-ה...';
+UPDATE "Show" SET "cast" = 'נתן וולקינד, רוני קידר, אבישג סרף' WHERE "slug" = 'אירלנד';
+UPDATE "Show" SET "cast" = 'ליליאן ברטו, יעל לבנטל, עידן אלתרמן / מעיין בלום, ליאור אביבי' WHERE "slug" = 'סביצ׳ה';
+UPDATE "Show" SET "cast" = 'רוי אברמוביץ׳, אביה סדי, גיא ויטנר, רוני שלו' WHERE "slug" = 'טובעים-השניים';
